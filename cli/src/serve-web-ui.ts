a0ad8fc814f8b71c3c1a5a1f249db12ba908bd37
Bell, Ryan <ryan.bell62101@gmail.com>
2026-04-26 00:42:18 -0700

    Add initial empty web UI

--- cli/src/serve-web-ui.ts
+++ cli/src/serve-web-ui.ts
@@ -5,7 +5,9 @@ export default async function serveWebUi(basePath: string, port: number) {
     port,
     async fetch(req) {
       const filePath = new URL(req.url).pathname;
-      const file = Bun.file(path.join(basePath, filePath));
+      const file = Bun.file(
+        path.join(basePath, filePath === "/" ? "index.html" : filePath),
+      );
       return new Response(file);
     },
     error() {
